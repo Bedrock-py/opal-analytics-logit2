56fc28a89740feeda9f8d867964a55c6b68b7e79
Trevor Goodyear <trevor.goodyear@gtri.gatech.edu>
2017-02-22 20:28:32 -0500

    Initial commit of Bedrock calling R script

---
+++ opals/logit2/logit2.R
@@ -0,0 +1,26 @@
+#Created by Pablo Diego Rosell, PhD, for Gallup inc. in February 2017
+#Stata logit2 emulation
+
+#Load data after preparing it in Stata
+
+# setwd("C:/Users/pablo_diego-rosell/Desktop/Projects/DARPA/Cycle 1/Research Protocols/Registration/Experiment 1/Rand2011PNAS_data_and_code")
+cooperation<-read.csv(commandArgs(TRUE)[1])
+
+install.packages("multiwayvcov")
+install.packages("lmtest")
+library ("multiwayvcov")
+library ("lmtest")
+
+#STATA LINES
+# char condition[omit] "Fluid"
+#   xi: logit2 decision i.condition  if round_num==1, fcluster(sessionnum) tcluster(playerid)
+
+#R EQUIVALENT
+
+cooperation$condition.f <- factor(cooperation$condition)
+cooperationR1<-subset(cooperation, round_num==1)
+logitR1 <- glm(decision0d1c ~ condition.f, data = cooperationR1, family = "binomial")
+logitR1.multiwayvcov <- cluster.vcov(logitR1, cbind(cooperationR1$sessionnum, cooperationR1$playerid))
+coeftest(logitR1, logitR1.multiwayvcov)
+
+
---
+++ opals/logit2/logit2.py
@@ -0,0 +1,46 @@
+#****************************************************************
+# Copyright (c) 2015, Georgia Tech Research Institute
+# All rights reserved.
+#
+# This unpublished material is the property of the Georgia Tech
+# Research Institute and is protected under copyright law.
+# The methods and techniques described herein are considered
+# trade secrets and/or confidential. Reproduction or distribution,
+# in whole or in part, is forbidden except by the express written
+# permission of the Georgia Tech Research Institute.
+#****************************************************************/
+import subprocess
+from bedrock.analytics.utils import Algorithm 
+
+
+class Logit2(Algorithm):
+    def __init__(self):
+        super(Kmeans, self).__init__()
+        self.parameters = ['']
+        self.inputs = ['matrix.csv']
+        self.outputs = ['matrix.csv']
+        self.name ='Logit2'
+        self.type = 'Logit'
+        self.description = 'Performs Logit2 the input dataset.'
+        self.parameters_spec = []
+
+    def compute(self, filepath, **kwargs):
+        path = filepath['matrix.csv']['rootdir'] + 'matrix.csv'
+
+
+        path = '/home/tgoodyear/projects/ngss/opal-analytics-logit2/Rand2011PNAS_cooperation_data.csv'
+        cmd = ['Rscript','logit2.R',path]
+        x = subprocess.check_output(cmd, universal_newlines=True)
+        tailStartChar = "Pr(>|z|)"
+        tailStart = x.find("Pr(>|z|)") + len(tailStartChar) + 4
+        tailEnd = x.find("---",tailStart)
+
+        # Estimate Std. Error z value Pr(>|z|)
+        resultHeader= ['Estimate','StdErr','zValue','Pr_z']
+        resultTable = [row.split() for row in x[tailStart:tailEnd].replace("*","").split('\n')]
+        result = resultHeader + resultTable
+
+        # print resultTable
+        # print tailStart, tailEnd
+
+        self.results = {'matrix.csv': result}
