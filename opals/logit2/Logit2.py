c3540cad2d6d35a9425e901fea5d81b08c57493b
Jason Poovey <japoovey@gmail.com>
2017-09-28 18:06:07 -0400

    Hacking in Logit to use R instead of python

--- opals/logit2/Logit2.py
+++ opals/logit2/Logit2.py
@@ -6,6 +6,9 @@ import pandas as pd
 import logging
 import statsmodels.api as sm
 import csv
+import rpy2.robjects as robjects
+from rpy2.robjects import r, pandas2ri
+from rpy2.robjects.packages import importr
 
 def check_valid_formula(formula):
     # TODO: Look at `patsy` for helper function to validate more fully
@@ -56,6 +59,38 @@ class Logit2(Algorithm):
     def compute(self, filepath, **kwargs):
         df = self.__build_df__(filepath)
 
+        pandas2ri.activate()
+        rstan = importr("Rcpp")
+        rstan = importr("sandwich")
+        rstan = importr("cluster")
+        rstan = importr("multiwayvcov")
+        rstan = importr("lme4")
+        rstan = importr("lmtest")
+        rstan = importr("broom")
+        rdf = pandas2ri.py2ri(df)
+        robjects.globalenv["rdf"] = rdf
+
+        rglmString = 'model <- glm({}, data = {}, family="{}")'.format(self.formula, "rdf", self.family)
+        model = r(rglmString)
+
+        if self.clustered_rse != "":
+            clusters = self.clustered_rse.split(",")
+            if len(clusters) == 1:
+                rvcovString = 'vcov <- cluster.vcov({}, cbind(rdf${}))'.format('model', clusters[0])
+            else:
+                rvcovString = 'vcov <- cluster.vcov({}, cbind(rdf${}, rdf${}))'.format('model', clusters[0], clusters[1])
+
+            vcov = r(rvcovString)
+
+            outputString = 'output <- coeftest({}, {})'.format('model', 'vcov')
+        else:
+            outputString = 'output <- coeftest({})'.format('model')
+
+        output = r(outputString)
+        out_df = r('out_df <- data.frame(tidy(output))')
+
+        self.results = {'matrix.csv': list(csv.reader(out_df.to_csv().split('\n'))), 'summary.csv': list(csv.reader(out_df.to_csv().split('\n')))}
+        """
         f = sm.families.Binomial()
 
         if self.family == "binomial":
@@ -96,3 +131,4 @@ class Logit2(Algorithm):
         ]
 
         self.results = {'matrix.csv': list(csv.reader(coef_table.to_csv().split('\n'))), 'summary.csv': model_summary}
+        """
